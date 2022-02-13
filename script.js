26f7af20afa993322a12bf36cb31b3f52fea1583
loubarb <louis.barbati@gmail.com>
2022-02-12 21:01:34 -0500

    Condensed javascript code and added keydown transitions

--- script.js
+++ script.js
@@ -1,39 +1,20 @@
-const audio = document.querySelector("audio");
-const key = document.querySelector(".key");
-const playing = document.querySelector(".playing");
 const keys = document.querySelectorAll(".key");
 
-document.addEventListener("keydown", function (e) {
-    if (e.code == 'KeyA') {
-        audio.currentTime = 0;
-        document.getElementById("clap").play();
-    } else if (e.code == 'KeyS') {
-        audio.currentTime = 0;
-        document.getElementById("hihat").play();
-    } else if (e.code == 'KeyD') {
-        audio.currentTime = 0;
-        document.getElementById("kick").play();
-    } else if (e.code == 'KeyF') {
-        audio.currentTime = 0;
-        document.getElementById("openhat").play();
-    } else if (e.code == 'KeyG') {
-        audio.currentTime = 0;
-        document.getElementById("boom").play();
-    } else if (e.code == 'KeyH') {
-        audio.currentTime = 0;
-        document.getElementById("ride").play();
-    } else if (e.code == 'KeyJ') {
-        audio.currentTime = 0;
-        document.getElementById("snare").play();
-    } else if (e.code == 'KeyK') {
-        audio.currentTime = 0;
-        document.getElementById("tom").play();
-    } else if (e.code == 'KeyL') {
-        audio.currentTime = 0;
-        document.getElementById("tink").play();
-    } else {
-        return false;
+document.addEventListener("keydown", function(e) {
+    const audio = document.querySelector(`audio[code="${e.code}"]`);
+    const key = document.querySelector(`.key[code="${e.code}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add("playing");
+})
+
+function removeTransition (e) {
+    if (e.propertyName !== "transform") {
+        return
     }
-});
+    this.classList.remove("playing"); //this = 'key'
+};
 
 
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
